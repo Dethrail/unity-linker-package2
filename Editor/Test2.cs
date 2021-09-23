e9cef3a2efa18d60a608b03222c4003b35f3285f
Maksym Slobodianiuk <max.od.ua@gmail.com>
2021-09-23 17:38:13 +0300

    up3

---
+++ Editor/Test2.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.UnityLinker;
+using UnityEngine;
+using UnityEditor;
+
+namespace Test.Editor
+{
+    public class LinkXmlInstaller : IUnityLinkerProcessor
+    {
+        int IOrderedCallback.callbackOrder => 0;
+        string IUnityLinkerProcessor.GenerateAdditionalLinkXmlFile(BuildReport report, UnityLinkerBuildPipelineData data)
+        {
+            // This is pretty ugly, but it was the only thing I could think of in order to reliably get the path to link.xml
+            const string linkXmlGuid = "a9121b83c8d271344a812e6978705a81"; // copied from link.xml.meta
+            var assetPath = AssetDatabase.GUIDToAssetPath(linkXmlGuid);
+            // assets paths are relative to the unity project root, but they don't correspond to actual folders for
+            // Packages that are embedded. I.e. it won't work if a package is installed as a git submodule
+            // So resolve it to an absolute path:
+            return Path.GetFullPath(assetPath);
+        }
+        void IUnityLinkerProcessor.OnBeforeRun(BuildReport report, UnityLinkerBuildPipelineData data)
+        {
+        }
+        void IUnityLinkerProcessor.OnAfterRun(BuildReport report, UnityLinkerBuildPipelineData data)
+        {
+        }
+    }
+}
+#endif
